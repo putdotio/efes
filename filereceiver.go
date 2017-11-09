6bf6d94f7603137a9a3b6079b23e8e318328ec3a
Cenk Alti <cenkalti@gmail.com>
2017-11-09 16:21:20 +0300

    implement file receiver

---
+++ filereceiver.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+var (
+	errOffset   = errors.New("offset mismatch")
+	errNotExist = errors.New("not valid upload")
+)
+
+// FileReceiver implements http.Handler for receiving files from clients in chunks.
+type FileReceiver struct {
+	dir string
+}
+
+func newFileReceiver(dir string) *FileReceiver {
+	return &FileReceiver{dir: dir}
+}
+
+func (f *FileReceiver) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := filepath.Join(f.dir, r.URL.Path)
+	switch r.Method {
+	case http.MethodPost:
+		err := createFile(path)
+		if err != nil {
+			log.Printf("cannot create file: %s", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	case http.MethodHead:
+		offset, err := getOffset(path)
+		if err == errNotExist {
+			log.Println("offset file does not exist")
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		if err != nil {
+			log.Printf("cannot get offset: %s", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("storage-file-offset", strconv.FormatInt(offset, 10))
+	case http.MethodPatch:
+		offset, err := strconv.ParseInt(r.Header.Get("storage-file-offset"), 10, 64)
+		if err != nil {
+			log.Printf("cannot parse offset: %s", err)
+			http.Error(w, "invalid header: storage-file-offset", http.StatusBadRequest)
+			return
+		}
+		err = saveFile(path, offset, r.Body)
+		if err == errOffset {
+			log.Printf("offset mismatch")
+			http.Error(w, "offset mismatch", http.StatusPreconditionFailed)
+			return
+		}
+		if err != nil {
+			log.Printf("cannot save file: %s", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	case http.MethodDelete:
+		err := deleteOffset(path)
+		if err == errNotExist {
+			log.Println("offset file does not exist")
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		if err != nil {
+			log.Printf("cannot delete offset: %s", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func createFile(path string) error {
+	f, err := os.Create(path)
+	if os.IsNotExist(err) {
+		err = os.MkdirAll(filepath.Dir(path), 0777)
+		if err != nil {
+			return err
+		}
+		f, err = os.Create(path)
+	}
+	if err != nil {
+		return err
+	}
+	err = f.Close()
+	if err != nil {
+		return err
+	}
+	return saveOffset(path, 0)
+}
+
+func getOffset(path string) (int64, error) {
+	b, err := ioutil.ReadFile(path + ".offset")
+	if os.IsNotExist(err) {
+		return 0, errNotExist
+	}
+	if err != nil {
+		return 0, err
+	}
+	return strconv.ParseInt(string(b), 10, 64)
+}
+
+func saveOffset(path string, offset int64) error {
+	return ioutil.WriteFile(path+".offset", []byte(strconv.FormatInt(offset, 10)), 0666)
+}
+
+func saveFile(path string, offset int64, r io.Reader) error {
+	fileOffset, err := getOffset(path)
+	if err != nil {
+		return err
+	}
+	if offset != fileOffset {
+		return errOffset
+	}
+	f, err := os.OpenFile(path, os.O_WRONLY, 0666)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+	n, err := io.Copy(f, r)
+	saveOffset(path, offset+n)
+	return err
+}
+
+func deleteOffset(path string) error {
+	err := os.Remove(path + ".offset")
+	if os.IsNotExist(err) {
+		return errNotExist
+	}
+	return err
+}
---
+++ filereceiver_test.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strconv"
+	"testing"
+)
+
+const path = "/dir/file.txt"
+
+func TestFileReceiver(t *testing.T) {
+	tempdir, err := ioutil.TempDir("", "storage-file-receiver-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tempdir)
+
+	fr := newFileReceiver(tempdir)
+
+	testOffset(t, fr, 404, false, 0)
+	testCreate(t, fr)
+	testOffset(t, fr, 200, true, 0)
+	testSend(t, fr, 0, "foo")
+	testOffset(t, fr, 200, true, 3)
+	testSend(t, fr, 3, "bar")
+	testOffset(t, fr, 200, true, 6)
+	testDelete(t, fr)
+	testOffset(t, fr, 404, false, 0)
+}
+
+func testCreate(t *testing.T, fr *FileReceiver) {
+	req, err := http.NewRequest("POST", path, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	fr.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Fatalf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+func testDelete(t *testing.T, fr *FileReceiver) {
+	req, err := http.NewRequest(http.MethodDelete, path, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("storage-file-offset", "6")
+	rr := httptest.NewRecorder()
+	fr.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Fatalf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+func testOffset(t *testing.T, fr *FileReceiver, statusCode int, checkValue bool, value int64) {
+	req, err := http.NewRequest("HEAD", path, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	fr.ServeHTTP(rr, req)
+	if status := rr.Code; status != statusCode {
+		t.Fatalf("handler returned wrong status code: got %v want %v", status, statusCode)
+	}
+	if checkValue {
+		offset, err := strconv.ParseInt(rr.Header().Get("storage-file-offset"), 10, 64)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if offset != value {
+			t.Fatalf("invalid offset: %d", offset)
+		}
+	}
+}
+
+func testSend(t *testing.T, fr *FileReceiver, offset int, data string) {
+	b := bytes.NewBufferString(data)
+	req, err := http.NewRequest("PATCH", path, b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("storage-file-offset", strconv.Itoa(offset))
+	rr := httptest.NewRecorder()
+	fr.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Fatalf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
