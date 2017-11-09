a82ba135c9875809bdb262f0232299f7ba006491
Cenk Alti <cenkalti@gmail.com>
2017-11-09 20:16:49 +0300

    refactor filereceiver tests

--- filereceiver_test.go
+++ filereceiver_test.go
@@ -12,40 +12,68 @@ import (
 
 const path = "/dir/file.txt"
 
-func TestFileReceiver(t *testing.T) {
-	tempdir, err := ioutil.TempDir("", "storage-file-receiver-test")
+var (
+	tempdir string
+	fr      *FileReceiver
+)
+
+func setup(t *testing.T) {
+	var err error
+	tempdir, err = ioutil.TempDir("", "storage-file-receiver-test")
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer os.RemoveAll(tempdir)
-
-	fr := newFileReceiver(tempdir)
-
-	testOffset(t, fr, 404, false, 0)
-	testCreate(t, fr)
-	testOffset(t, fr, 200, true, 0)
-	testSend(t, fr, 0, false, "foo")
-	testOffset(t, fr, 200, true, 3)
-	testSend(t, fr, 3, false, "bar")
-	testOffset(t, fr, 200, true, 6)
-	testDelete(t, fr)
-	testOffset(t, fr, 404, false, 0)
-
-	// patch without post
-	testSend(t, fr, 0, false, "baz")
-	testOffset(t, fr, 200, true, 3)
-	testDelete(t, fr)
-	testOffset(t, fr, 404, false, 0)
-
-	// send file length
-	testOffset(t, fr, 404, false, 0)
-	testSend(t, fr, 0, true, "baz")
-	testOffset(t, fr, 404, false, 0)
-
-	// TODO test 0 byte files
+	fr = newFileReceiver(tempdir)
 }
 
-func testCreate(t *testing.T, fr *FileReceiver) {
+func tearDown() {
+	os.RemoveAll(tempdir)
+}
+
+func TestFileReceiver(t *testing.T) {
+	setup(t)
+	defer tearDown()
+
+	testOffset(t, 404, false, 0)
+	testCreate(t)
+	testOffset(t, 200, true, 0)
+	testSend(t, 0, false, "foo")
+	testOffset(t, 200, true, 3)
+	testSend(t, 3, false, "bar")
+	testOffset(t, 200, true, 6)
+	testDelete(t)
+	testOffset(t, 404, false, 0)
+}
+
+func TestFileReceiverNoCreate(t *testing.T) {
+	setup(t)
+	defer tearDown()
+
+	testSend(t, 0, false, "baz")
+	testOffset(t, 200, true, 3)
+	testDelete(t)
+	testOffset(t, 404, false, 0)
+}
+
+func TestFileReceiverNoDelete(t *testing.T) {
+	setup(t)
+	defer tearDown()
+
+	testCreate(t)
+	testSend(t, 0, true, "baz")
+	testOffset(t, 404, false, 0)
+}
+
+func TestFileReceiverZeroByte(t *testing.T) {
+	setup(t)
+	defer tearDown()
+
+	// TODO test 0 byte files
+	t.SkipNow()
+}
+
+func testCreate(t *testing.T) {
+	t.Helper()
 	req, err := http.NewRequest("POST", path, nil)
 	if err != nil {
 		t.Fatal(err)
@@ -57,7 +85,8 @@ func testCreate(t *testing.T, fr *FileReceiver) {
 	}
 }
 
-func testDelete(t *testing.T, fr *FileReceiver) {
+func testDelete(t *testing.T) {
+	t.Helper()
 	req, err := http.NewRequest(http.MethodDelete, path, nil)
 	if err != nil {
 		t.Fatal(err)
@@ -70,7 +99,8 @@ func testDelete(t *testing.T, fr *FileReceiver) {
 	}
 }
 
-func testOffset(t *testing.T, fr *FileReceiver, statusCode int, checkValue bool, value int64) {
+func testOffset(t *testing.T, statusCode int, checkValue bool, value int64) {
+	t.Helper()
 	req, err := http.NewRequest("HEAD", path, nil)
 	if err != nil {
 		t.Fatal(err)
@@ -91,7 +121,8 @@ func testOffset(t *testing.T, fr *FileReceiver, statusCode int, checkValue bool,
 	}
 }
 
-func testSend(t *testing.T, fr *FileReceiver, offset int, sendLength bool, data string) {
+func testSend(t *testing.T, offset int, sendLength bool, data string) {
+	t.Helper()
 	b := bytes.NewBufferString(data)
 	req, err := http.NewRequest("PATCH", path, b)
 	if err != nil {
