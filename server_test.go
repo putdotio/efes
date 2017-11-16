cd89aa9b5afab7044d09bc2d3fdd252717ad3080
Omer Murat Yildirim <omermuratyildirim@gmail.com>
2017-11-16 15:31:37 +0300

    Separate tests

--- server_test.go
+++ server_test.go
@@ -5,14 +5,13 @@ import (
 	"time"
 )
 
-func TestFidExistsOnDatabase(t *testing.T) {
+func setupServer(t *testing.T, fileID int64) *Server {
 	s, err := NewServer(testConfig)
 	if err != nil {
 		t.Fatal(err)
 	}
 	cleanDB(t, s.db)
-	// Insert into file table
-	_, err = s.db.Exec("insert into file(fid, dmid, classid, devcount) values(1, 1, 1, 1)")
+	_, err = s.db.Exec("insert into file(fid, dmid, classid, devcount) values(?, 1, 1, 1)", fileID)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -21,6 +20,12 @@ func TestFidExistsOnDatabase(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	return s
+
+}
+
+func TestFidExistsOnDatabase(t *testing.T) {
+	s := setupServer(t, 1)
 
 	res, err := s.fidExistsOnDatabase(1)
 	if err != nil {
@@ -32,26 +37,10 @@ func TestFidExistsOnDatabase(t *testing.T) {
 	}
 }
 
-func TestShouldDeleteFile(t *testing.T) {
+func TestShouldDeleteFileExistsOnDbNewOnDisk(t *testing.T) {
 	testConfig.Server.CleanDiskFileTTL = 300
-	s, err := NewServer(testConfig)
-	if err != nil {
-		t.Fatal(err)
-	}
+	s := setupServer(t, 1)
 
-	cleanDB(t, s.db)
-	// Insert into file table
-	_, err = s.db.Exec("insert into file(fid, dmid, classid, devcount) values(1, 1, 1, 1)")
-	if err != nil {
-		t.Fatal(err)
-	}
-	// Insert into tempfile table
-	_, err = s.db.Exec("insert into tempfile(fid, createtime, classid, dmid) values(1, 1501245392, 1, 1)")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Case-1: File exist on db && new on disk
 	modTime := time.Now().Add(-time.Duration(200 * time.Second))
 	res, err := s.shouldDeleteFile(1, modTime)
 	if err != nil {
@@ -62,9 +51,13 @@ func TestShouldDeleteFile(t *testing.T) {
 		t.Error("File exists on database but return not-exists!")
 	}
 
-	// Case-2: File exist on db && old on disk
-	modTime = time.Now().Add(-time.Duration(400 * time.Second))
-	res, err = s.shouldDeleteFile(1, modTime)
+}
+func TestShouldDeleteFileExistsOnDbOldOnDisk(t *testing.T) {
+	testConfig.Server.CleanDiskFileTTL = 300
+	s := setupServer(t, 1)
+
+	modTime := time.Now().Add(-time.Duration(400 * time.Second))
+	res, err := s.shouldDeleteFile(1, modTime)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -72,10 +65,13 @@ func TestShouldDeleteFile(t *testing.T) {
 	if res {
 		t.Error("File exists on database but return not-exists!")
 	}
+}
+func TestShouldDeleteFileNotExistsOnDbNewOnDisk(t *testing.T) {
+	testConfig.Server.CleanDiskFileTTL = 300
+	s := setupServer(t, 1)
 
-	// Case-3: File not exist on db && new on disk
-	modTime = time.Now().Add(-time.Duration(200 * time.Second))
-	res, err = s.shouldDeleteFile(2, modTime)
+	modTime := time.Now().Add(-time.Duration(200 * time.Second))
+	res, err := s.shouldDeleteFile(2, modTime)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -83,10 +79,13 @@ func TestShouldDeleteFile(t *testing.T) {
 	if res {
 		t.Error("File exists on database but return not-exists!")
 	}
+}
+func TestShouldDeleteFileNotExistsOnDbOldOnDisk(t *testing.T) {
+	testConfig.Server.CleanDiskFileTTL = 300
+	s := setupServer(t, 1)
 
-	// Case-4: File not exist on db && old on disk
-	modTime = time.Now().Add(-time.Duration(400 * time.Second))
-	res, err = s.shouldDeleteFile(2, modTime)
+	modTime := time.Now().Add(-time.Duration(400 * time.Second))
+	res, err := s.shouldDeleteFile(2, modTime)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -94,4 +93,5 @@ func TestShouldDeleteFile(t *testing.T) {
 	if !res {
 		t.Error("File should be deleted but returned not to!")
 	}
+
 }
