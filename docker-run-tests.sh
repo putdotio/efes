bd30ef7dcd873c7f524c34dede8ee7e48f129bc5
Omer Murat Yildirim <omermuratyildirim@gmail.com>
2017-11-21 12:00:44 +0300

    RabbitMQ setup for tests

--- docker-run-tests.sh
+++ docker-run-tests.sh
@@ -7,6 +7,30 @@ until "${mysql[@]}" -e "select 1" &>/dev/null ; do
 done
 echo "MySQL is ready."
 
-# TODO wait for RabbitMQ to be ready
+echo Creating data directory...
+RABBITMQ_DATADIR=/var/lib/rabbitmq2
+mkdir -p "$RABBITMQ_DATADIR"
+
+echo Starting RabbitMQ server...
+rabbitmq-server --hostname localhost &>/dev/null &
+pid="$!"
+
+echo Waiting for the server to get ready...
+for i in {30..0}; do
+        if rabbitmqctl -t 1 list_queues &> /dev/null; then
+                break
+        fi
+        echo 'RabbitMQ is not ready yet...'
+        sleep 1
+done
+if [ "$i" = 0 ]; then
+        echo >&2 'RabbitMQ init process failed.'
+        exit 1
+fi
+
+echo Adding user...
+rabbitmqctl add_user efes 123
+rabbitmqctl set_user_tags efes administrator
+rabbitmqctl set_permissions -p / efes  ".*" ".*" ".*"
 
 exec go test -v ./...
