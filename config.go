96d9477604e2f4e4d60185086b17cff2765d9919
Cenk Altı <cenkalti@gmail.com>
2017-11-30 15:00:33 +0300

    parse durations in config (#4)

--- config.go
+++ config.go
@@ -1,12 +1,16 @@
 package main
 
-import "github.com/BurntSushi/toml"
+import (
+	"time"
+
+	"github.com/BurntSushi/toml"
+)
 
 // TrackerConfig holds configuration values for Tracker.
 type TrackerConfig struct {
-	ListenAddress   string `toml:"listen_address"`
-	ShutdownTimeout uint32 `toml:"shutdown_timeout"`
-	TempfileTooOld  uint32 `toml:"tempfile_too_old"`
+	ListenAddress   string   `toml:"listen_address"`
+	ShutdownTimeout Duration `toml:"shutdown_timeout"`
+	TempfileTooOld  Duration `toml:"tempfile_too_old"`
 }
 
 // DatabaseConfig holds configuration values for database.
@@ -21,19 +25,19 @@ type AMQPConfig struct {
 
 // ServerConfig holds configuration values for Server.
 type ServerConfig struct {
-	DataDir              string `toml:"datadir"`
-	ListenAddress        string `toml:"listen_address"`
-	ListenAddressForRead string `toml:"listen_address_for_read"`
-	ShutdownTimeout      uint32 `toml:"shutdown_timeout"`
-	CleanDiskRunPeriod   int    `toml:"clean_disk_run_period"`
-	CleanDiskFileTTL     int    `toml:"clean_disk_file_ttl"`
+	DataDir              string   `toml:"datadir"`
+	ListenAddress        string   `toml:"listen_address"`
+	ListenAddressForRead string   `toml:"listen_address_for_read"`
+	ShutdownTimeout      Duration `toml:"shutdown_timeout"`
+	CleanDiskRunPeriod   Duration `toml:"clean_disk_run_period"`
+	CleanDiskFileTTL     Duration `toml:"clean_disk_file_ttl"`
 }
 
 // ClientConfig holds configuration values for Client.
 type ClientConfig struct {
 	TrackerURL   string    `toml:"tracker_url"`
 	ChunkSize    ChunkSize `toml:"chunk_size"`
-	SendTimeout  uint32    `toml:"send_timeout"`
+	SendTimeout  Duration  `toml:"send_timeout"`
 	ShowProgress bool      `toml:"show_progress"`
 }
 
@@ -50,21 +54,21 @@ type Config struct {
 var defaultConfig = Config{
 	Tracker: TrackerConfig{
 		ListenAddress:   "0.0.0.0:8001",
-		ShutdownTimeout: 3000,
-		TempfileTooOld:  86400000,
+		ShutdownTimeout: Duration(3 * time.Second),
+		TempfileTooOld:  Duration(24 * time.Hour),
 	},
 	Server: ServerConfig{
 		DataDir:              "/srv/efes/dev1",
 		ListenAddress:        "0.0.0.0:8501",
 		ListenAddressForRead: "0.0.0.0:8500",
-		ShutdownTimeout:      10000,
-		CleanDiskFileTTL:     43200,
-		CleanDiskRunPeriod:   259200,
+		ShutdownTimeout:      Duration(10 * time.Second),
+		CleanDiskFileTTL:     Duration(24 * time.Hour),
+		CleanDiskRunPeriod:   Duration(7 * 24 * time.Hour),
 	},
 	Client: ClientConfig{
 		TrackerURL:   "http://127.0.0.1:8001",
-		ChunkSize:    100 * 1024 * 1024,
-		SendTimeout:  10,
+		ChunkSize:    50 * M,
+		SendTimeout:  Duration(10 * time.Second),
 		ShowProgress: true,
 	},
 	Database: DatabaseConfig{
