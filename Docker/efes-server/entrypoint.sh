4d8160a86fde2ddaafe841742a05a452f5538391
Omer Murat Yildirim <omermuratyildirim@gmail.com>
2025-01-21 10:41:53 +0300

    docker: add volumes

--- Docker/efes-server/entrypoint.sh
+++ Docker/efes-server/entrypoint.sh
@@ -8,10 +8,22 @@ datadir=/srv/efes/dev$devid
 mkdir -p $datadir
 ip=$(hostname -i)
 
-sqlzone="REPLACE INTO zone (zoneid, name) VALUES ($zoneid, 'zone$zoneid')"
-sqlrack="REPLACE INTO rack (rackid, zoneid, name) VALUES ($rackid, $zoneid, 'rack$rackid')"
-sqlhost="REPLACE INTO host (hostid, rackid, status, hostname, hostip) VALUES ($hostid, $rackid, 'alive', '$HOSTNAME', '$ip')"
-sqldevice="REPLACE INTO device (devid, hostid, status) VALUES ($devid, $hostid, 'alive')"
+sqlzone="INSERT INTO zone (zoneid, name)
+         VALUES ($zoneid, 'zone$zoneid')
+         ON DUPLICATE KEY UPDATE name = VALUES(name)"
+
+sqlrack="INSERT INTO rack (rackid, zoneid, name)
+         VALUES ($rackid, $zoneid, 'rack$rackid')
+         ON DUPLICATE KEY UPDATE zoneid = VALUES(zoneid), name = VALUES(name)"
+
+sqlhost="INSERT INTO host (hostid, rackid, status, hostname, hostip)
+         VALUES ($hostid, $rackid, 'alive', '$HOSTNAME', '$ip')
+         ON DUPLICATE KEY UPDATE rackid = VALUES(rackid), status = VALUES(status),
+                                hostname = VALUES(hostname), hostip = VALUES(hostip)"
+
+sqldevice="INSERT INTO device (devid, hostid, status)
+           VALUES ($devid, $hostid, 'alive')
+           ON DUPLICATE KEY UPDATE hostid = VALUES(hostid), status = VALUES(status)"
 
 efes ready mysql --exec "$sqlzone"
 efes ready mysql --exec "$sqlrack"
